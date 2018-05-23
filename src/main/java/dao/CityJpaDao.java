2364112343a059d8c486c5e1166fc08b2f81224e
Emmanuelle Contardo <emmanuelle.contardo.pf1@gmail.com>
2018-05-23 08:45:56 +0200

    MAJ

--- src/main/java/dao/CityJpaDao.java
+++ src/main/java/dao/CityJpaDao.java
@@ -6,10 +6,11 @@ import co.simplon.jpaEtHibernate.City;
 
 public class CityJpaDao implements CityDao {
 	private EntityManager em;
-	public CityJpaDao (EntityManager em) {
+
+	public CityJpaDao(EntityManager em) {
 		this.em = em;
 	}
-	
+
 	@Override
 	public City createCity(City city) {
 		em.getTransaction().begin();
@@ -20,21 +21,36 @@ public class CityJpaDao implements CityDao {
 
 	@Override
 	public City getCityById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		City city = em.find(City.class, id);
+		// em.close();
+		return city;
+
 	}
 
 	@Override
 	public City updateCity(City city) {
-		// TODO Auto-generated method stub
-		return null;
+		em.getTransaction().begin();
+		city = em.merge(city);
+		em.getTransaction().commit();
+		return city;
 	}
 
 	@Override
 	public void deleteCityById(Long id) {
-		// TODO Auto-generated method stub
-		
+		em.getTransaction().begin();
+		 City city = em.find(City.class, id);
+		 if (city != null) {
+		 em.remove(city);
+		 }
+		 em.getTransaction().commit();
+		 }
+// em.getTransaction().begin();
+		// City city = readCity(em, 18L);
+		// em.remove(city);
+		// em.getTransaction().commit();
+		// em.close();
+	
+	
 	}
 
-	
-}
+
